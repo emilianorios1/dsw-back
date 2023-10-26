adf388c8872752a6ec5648a0e49e02cea038e93d
Tute <mateoyastor60@gmail.com>
2023-10-26 20:23:34 -0300

    feat: added car api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import publicationsRoutes from './routes/publications'
 import categoriesRoutes from './routes/categories'
+import carsRoutes from './routes/cars'
 
 const app = express()
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(cors())
 
 app.use('/api', publicationsRoutes);
 app.use('/api', categoriesRoutes);
+app.use('/api', carsRoutes);
 
 
 app.listen(5000)
