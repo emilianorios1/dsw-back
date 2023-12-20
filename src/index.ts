63e6e92f4ee8243a613d4dcae266078deb6256e3
emilianorios1 <emilianorios99@gmail.com>
2023-12-20 01:46:01 -0300

    fix: added claimIncludes

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,24 @@ import carModels from './routes/carModels'
 import carPublications from './routes/carPublications'
 import planeModels from './routes/planeModels'
 import planePublications from './routes/planePublications'
-import { auth, requiredScopes } from 'express-oauth2-jwt-bearer'
+import { auth, claimIncludes, requiredScopes } from 'express-oauth2-jwt-bearer'
 
 const checkJwt = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
 });
-
-
-//const checkAdmin = requiredScopes('admin')
+const checkAdmin = claimIncludes('permissions', 'admin')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', checkJwt, brands);
-app.use('/api', boatModels);
+app.use('/api', checkJwt, checkAdmin, brands);
+app.use('/api', checkJwt, checkAdmin, boatModels);
 app.use('/api', boatPublications);
-app.use('/api', carModels);
+app.use('/api', checkJwt, checkAdmin, carModels);
 app.use('/api', carPublications)
-app.use('/api', planeModels)
+app.use('/api', checkJwt, checkAdmin, planeModels)
 app.use('/api', planePublications)
 
 
