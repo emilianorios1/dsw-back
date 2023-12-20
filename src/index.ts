926f83f3297d0637c37feec9915da088f9666ab8
emilianorios1 <emilianorios99@gmail.com>
2023-12-20 01:39:17 -0300

    feat: added checkJwt with express-oauth2-jwt-bearer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction } from 'express'
 import cors from 'cors'
 import brands from './routes/brands'
 import boatModels from './routes/boatModels'
@@ -7,14 +7,21 @@ import carModels from './routes/carModels'
 import carPublications from './routes/carPublications'
 import planeModels from './routes/planeModels'
 import planePublications from './routes/planePublications'
-import { checkJwt } from './middleware/middleware';
+import { auth, requiredScopes } from 'express-oauth2-jwt-bearer'
 
+const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+});
+
+
+//const checkAdmin = requiredScopes('admin')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', brands);
+app.use('/api', checkJwt, brands);
 app.use('/api', boatModels);
 app.use('/api', boatPublications);
 app.use('/api', carModels);
@@ -26,3 +33,4 @@ app.use('/api', planePublications)
 app.listen(5000)
 console.log('Server on port ', 5000)
 
+ 
